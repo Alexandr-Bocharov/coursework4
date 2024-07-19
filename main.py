2d65d503dd18df01abe1bb2b11b0f13596357a11
tanjiro <counter23062001@mail.ru>
2024-07-19 17:03:38 +0300

    move superfluous to src.user_interaction

--- main.py
+++ main.py
@@ -1,35 +1,4 @@
-import requests
-import json
-from src.json_saver import JSONSaver
-from src.vacancy import Vacancy
-from src.hh import HeadHunterAPI
-from src.utils import (search_query,
-                       clear_json,
-                       # get_vacancies_by_salary,
-                       # filter_vacancies,
-                       sort_vacancies,
-                       get_top_vacancies,
-                       print_vacancies)
-
-hh1 = HeadHunterAPI()
-hh1_vacancies = hh1.get_vacancies('python')
-vacancies_list = Vacancy.cast_to_object_list(hh1_vacancies)
-
-json_saver = JSONSaver('vacancies.json')
-
-
-def user_interaction():
-    platforms = ["HeadHunter"]
-    keywords_for_search = input("Введите поисковый запрос: ").split()
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-
-
-    filtered_vacancies = search_query(keywords_for_search)
-
-    top_vacancies = get_top_vacancies(filtered_vacancies, top_n)
-    sorted_vacancies = sort_vacancies(top_vacancies)
-    print_vacancies(sorted_vacancies)
-
+from src.user_interaction import user_interaction
 
 if __name__ == '__main__':
     user_interaction()
